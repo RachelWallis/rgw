391d1865e3d14644499f2d28e9f2a03fb3709b37
Rachel Wallis <rachelwalliswork@gmail.com>
2025-04-05 19:14:11 +0100

    Fix: move global SCSS import to root layout.js

--- src/app/(defaultLayout)/layout.jsx
+++ src/app/(defaultLayout)/layout.jsx
@@ -3,7 +3,6 @@
 import React, { useEffect } from 'react';
 import Navbar from '../Components/Navs/Nav';
 import Footer from '../Components/Misc/Footer';
-import '../assets/sass/style.scss';
 
 const DefaultLayout = ({ children }) => {
     return (
