c282b7542711eac2070d9b3a375cbed409488dd5
Rachel Wallis <rachelwalliswork@gmail.com>
2025-04-05 19:06:20 +0100

    Fix: corrected sass import

--- src/app/(defaultLayout)/layout.jsx
+++ src/app/(defaultLayout)/layout.jsx
@@ -3,9 +3,9 @@
 import React, { useEffect } from 'react';
 import Navbar from '../Components/Navs/Nav';
 import Footer from '../Components/Misc/Footer';
+import '../assets/sass/style.scss';
 
 const DefaultLayout = ({ children }) => {
-
     return (
         <div className='main-page-area'>
             <Navbar />
