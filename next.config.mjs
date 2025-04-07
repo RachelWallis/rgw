972f1bc91ea54302d9bf0f0faa7081457491ee56
Rachel Wallis <rachelwalliswork@gmail.com>
2025-04-07 17:46:56 +0100

    Fix: changed next config to fix production issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 export default {
     output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
     trailingSlash: true,
-    basePath: process.env.NODE_ENV === 'production' ? '/weaverwebs-nextjs' : undefined,
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/weaverwebs-nextjs' : undefined,
     images: {
         unoptimized: true
     }
